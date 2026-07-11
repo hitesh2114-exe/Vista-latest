bf6baefcca4808e29e374da6f5a52a36beab04fb
hitesh2114-exe <hiteshpandey2114@gmail.com>
2026-07-11 11:57:54 +0530

    changes made

--- Backend/index.js
+++ Backend/index.js
@@ -86,6 +86,8 @@ const sessionOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    secure: true,
+    sameSite: "none",
   },
 };
 
